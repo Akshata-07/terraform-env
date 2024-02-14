5410a49b8cdeceb82c1c11c2955a6122587b30a2
akshata <akshata@123>
2024-02-14 22:19:47 +0530

    bucket created

--- Tasks-v1/user.tf
+++ Tasks-v1/user.tf
@@ -1,3 +1,7 @@
+# 1. created IAM user in AWS using terraform 
+
+/*
+
 provider "aws"{
     region = "eu-north-1"
 }
@@ -6,5 +10,21 @@ resource "aws_iam_user" "user-name" {
     name = var.user-name
     tags = {
         env = var.env
+    }
+}
+
+*/
+
+# 2. created s3 bucket in AWS using terraform 
+
+provider "aws" {
+    region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+    bucket = var.bucket-name
+    tags = {
+        Name = "s3-bucket"
+        env = var.bucket-env
     }
 }
--- Tasks-v1/variable.tf
+++ Tasks-v1/variable.tf
@@ -5,3 +5,12 @@ variable "user-name" {
 variable "env" {
     default = "dev"
 }
+
+
+variable "bucket-name" {
+    default = "myyyyybuckettttttt"
+}
+
+variable "bucket-env" {
+    default = "storage"
+}
