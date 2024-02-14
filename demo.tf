5cf1968ed6086c4cc51e8691d2e687eea4a18be9
akshata <akshata@123>
2024-02-14 17:20:26 +0530

    hdg

--- demo.tf
+++ demo.tf
@@ -1,7 +1,5 @@
-/*
-
 provider "aws" {
-    region = "us-east-1"
+    region = "eu-north-1"
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -13,87 +11,86 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "private_subnet" {
-    vpc_id     = aws_vpc.my_vpc.id
+    vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.private_subnet_cidr
 
     tags = {
-        env = var.env
-        Name = "${var.project}-private-subnet"
+      Name = "${var.project}-priavte-subnet"
+      env = var.env
     }
 }
 
 resource "aws_subnet" "public_subnet" {
-    vpc_id     = aws_vpc.my_vpc.id
+    vpc_id = aws_vpc.my_vpc.id
     cidr_block = var.public_subnet_cidr
     map_public_ip_on_launch = true
     tags = {
-        env = var.env
         Name = "${var.project}-public-subnet"
     }
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "internet_gateway" "my_igw" {
+    vpc_id = aws_vpc.my_vpc.id
 
-  tags = {
+    tags = {
     env = var.env
     Name = "${var.project}-igw"
   }
 }
 
 resource "aws_route" "igw_route" {
-  route_table_id            = aws_vpc.my_vpc.default_route_table_id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.my_igw.id
+    route_table_id = aws_vpc.my_vpc.default_route_table_id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = internet_gateway.my_igw.id
 }
 
 resource "aws_security_group" "my_sg" {
-    Name = "${var.project}-sg"
-    description = "allow http and ssh"
+    name = "${var.project}-sg"
+    description = "allow httpd and ssh"
     vpc_id = aws_vpc.my_vpc.id
+
     ingress {
         cidr_blocks = ["0.0.0.0/0"]
         protocol = "TCP"
         from_port = 22
         to_port = 22
     }
+
     ingress {
         cidr_blocks = ["0.0.0.0/0"]
         protocol = "TCP"
         from_port = 80
         to_port = 80
     }
+
     egress {
         cidr_blocks = ["0.0.0.0/0"]
         protocol = "-1"
         from_port = 0
         to_port = 0
     }
-    depends_on = [aws_internet_gateway.my_igw]
-}
 
-resource "aws_instance" "instance_1" {
+}
+resource "aws_instance" "instance-1" {
     ami = var.image_id
     instance_type = var.instance_type
-    key_Name = var.key_pair
+    key_name = var.key_pair
     vpc_security_group_ids = [aws_security_group.my_sg.id]
     tags = {
-        Name = "${var.project}-private-instance"
-        env = var.env
+      Name = "${var.project}-private-instance"
+      env = var.env
     }
     subnet_id = aws_subnet.private_subnet.id
 }
 
-resource "aws_instance" "instance_2" {
+resource "aws_instance" "instance-2" {
     ami = var.image_id
     instance_type = var.instance_type
-    key_Name = var.key_pair
+    key_name = var.key_pair
     vpc_security_group_ids = [aws_security_group.my_sg.id]
     tags = {
-        Name = "${var.project}-public-instance"
-        env = var.env
+      Name = "${var.project}-public-instance"
+      env = var.env
     }
     subnet_id = aws_subnet.public_subnet.id
 }
-
-*/
