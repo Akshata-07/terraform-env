7ba09d2cefa352a9ac300b116aebf046de37990d
akshata <akshata@123>
2024-02-21 13:53:29 +0530

    vv

--- cluster-creation/main.tf
+++ cluster-creation/main.tf
@@ -1,34 +1,6 @@
-provider "aws"{
+provider "aws" {
     region = "eu-north-1"
 }
-/*
-resource "aws_iam_role" "eks_role" {
-  name = "eks-cluster-role"
-   assume_role_policy = <<POLICY
-    {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": {
-                    "Service": [
-                        "eks.amazonaws.com"
-                    ]
-                },
-                "Action": "sts:AssumeRole"
-            }
-        ]
-    }
-    POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-*/
-
 
 data "aws_iam_policy_document" "assume_role" {
   statement {
