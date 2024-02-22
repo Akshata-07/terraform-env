6d427361e3b638978a5a450c189d99c55515701f
akshata <akshata@123>
2024-02-22 18:08:31 +0530

    attachments added

--- cluster-creation/main.tf
+++ cluster-creation/main.tf
@@ -121,6 +121,23 @@ resource "aws_iam_role" "noderole" {
   })
 }
 
+#attaching role policy and nodes
+resource "aws_iam_role_policy_attachment" "my-node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.noderole.name
+}
+
+resource "aws_iam_role_policy_attachment" "my-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.noderole.name
+}
+
+resource "aws_iam_role_policy_attachment" "my-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.noderole.name
+}
+
+
 #creating nodes
 resource "aws_eks_node_group" "my-node" {
   cluster_name = aws_eks_cluster.my_cluster.name
