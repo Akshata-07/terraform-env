1f068dbb8101450d5af2a936b620d90ce9c0cab8
akshata <akshata@123>
2024-02-22 22:59:27 +0530

    fhbvf

--- cluster-creation/main.tf
+++ cluster-creation/main.tf
@@ -168,10 +168,6 @@ resource "aws_eks_node_group" "my-node" {
   ]
 }
 
-resource "aws_nat_gateway" "my_nat_gateway" {
-  allocation_id = aws_eip.my_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
-}
 
 resource "aws_eip" "nat_gateway_eip" {
   domain = "vpc"
